261aa19b01516279d25f02ab77dab564a37d9e5f
flwrsofvl <kalmamatovtimur0@gmail.com>
2026-05-16 19:28:27 +0600

    Добавлена генерация числа и проверка

--- src/project3/GuessNumberApp.java
+++ src/project3/GuessNumberApp.java
@@ -1,15 +1,22 @@
 package project3;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
 import javax.swing.*;
 
-public class GuessNumberApp extends JFrame {
+public class GuessNumberApp extends JFrame implements ActionListener {
 
     JLabel labelTitle = new JLabel("Я загадал число от 1 до 100.");
     JLabel labelStatus = new JLabel("Введите число и нажмите кнопку.");
     JLabel labelAttempts = new JLabel("Попыток: 0");
     JTextField inputField = new JTextField(10);
     JButton btnCheck = new JButton("Проверить");
+
+    int targetNumber;
+    int count = 0;
+    Random rand = new Random();
 
     public GuessNumberApp() {
         setTitle("Угадай число");
@@ -26,6 +33,26 @@ public class GuessNumberApp extends JFrame {
         add(inputPanel);
         add(labelStatus);
         add(labelAttempts);
+
+        btnCheck.addActionListener(this);
+        targetNumber = rand.nextInt(100) + 1;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String text = inputField.getText();
+        int userNum = Integer.parseInt(text);
+
+        count++;
+        labelAttempts.setText("Попыток: " + count);
+
+        if (userNum == targetNumber) {
+            labelStatus.setText("Вы угадали! Это число " + targetNumber);
+        } else if (userNum < targetNumber) {
+            labelStatus.setText("Мало! Загаданное число больше.");
+        } else {
+            labelStatus.setText("Много! Загаданное число меньше.");
+        }
     }
 
     public static void main(String[] args) {
